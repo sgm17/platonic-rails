cc4042e687113676a0ac5aa3cfeaaacfa6a986d6
Ubuntu <ubuntu@ip-172-31-33-64.eu-west-3.compute.internal>
2023-03-29 14:27:01 +0000

    modify cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-every :day, at: '4pm', timezone: 'Pacific Time (US & Canada)' do
-    runner "MeetsController.create_meetings"
+every :day, at: '2:16pm', timezone: 'Pacific Time (US & Canada)' do
+	command "/home/ubuntu/.rbenv/shims/ruby /home/ubuntu/platonic-rails/app/bin/rails runner -e production 'Api::V1::MeetsController.create_meetings'"
 end
