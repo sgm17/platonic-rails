67a98d1a62affb12cbd94a9e9d5bcbb84a0c2cbe
Sergi <sergigm78@gmail.com>
2023-04-02 12:07:10 +0200

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,6 @@
 
 set :output, 'log/schedule.log'
 
-#every :day, at: '1pm', timezone: 'Pacific Time (US & Canada)' do
-every 1.minute do
+every :day, at: '1pm', timezone: 'Pacific Time (US & Canada)' do
 	runner "Api::V1::MeetsController.create_meetings"
 end
